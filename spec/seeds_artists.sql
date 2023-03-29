568c603c19e003642574cbe62b970efcdcc4e22d
Jack <jwleggett@outlook.com>
2023-03-29 14:31:38 +0100

    artists finished

---
+++ spec/seeds_artists.sql
@@ -0,0 +1,8 @@
+
+TRUNCATE TABLE artists RESTART IDENTITY; -- replace with your own table name.
+
+-- Below this line there should only be `INSERT` statements.
+-- Replace these statements with your own seed data.
+
+INSERT INTO artists (name, genre) VALUES ('Kanye west', 'hip-hop');
+INSERT INTO artists (name, genre) VALUES ('Mac Miller', 'rap');
